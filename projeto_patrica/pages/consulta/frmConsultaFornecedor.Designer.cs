c4a8e0cbeee39761adb1f835026ebe969508a5c3
Luan Henrique Oliveira Sales <LuanHos1998@hotmail.com>
2025-03-27 03:56:24 -0300

    .

--- projeto_patrica/pages/consulta/frmConsultaFornecedor.Designer.cs
+++ projeto_patrica/pages/consulta/frmConsultaFornecedor.Designer.cs
@@ -28,7 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.clmTipo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmNomeRazaoSocial = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmApelidoNomeFantasia = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmGenero = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmDataNascimentoCriacao = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmCpfCnpj = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmRgInscEstadual = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmEmail = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmTelefone = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmEndereco = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmBairro = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmCidade = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmCep = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmAtivo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmInscMunicipal = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmInscEstSubTrib = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
+            // 
+            // listV
+            // 
+            this.listV.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.clmTipo,
+            this.clmNomeRazaoSocial,
+            this.clmApelidoNomeFantasia,
+            this.clmGenero,
+            this.clmDataNascimentoCriacao,
+            this.clmCpfCnpj,
+            this.clmRgInscEstadual,
+            this.clmEmail,
+            this.clmTelefone,
+            this.clmEndereco,
+            this.clmBairro,
+            this.clmCidade,
+            this.clmCep,
+            this.clmAtivo,
+            this.clmInscMunicipal,
+            this.clmInscEstSubTrib});
+            // 
+            // clmTipo
+            // 
+            this.clmTipo.Text = "Tipo de Pessoa";
+            // 
+            // clmNomeRazaoSocial
+            // 
+            this.clmNomeRazaoSocial.Text = "Nome/Razão Social";
+            // 
+            // clmApelidoNomeFantasia
+            // 
+            this.clmApelidoNomeFantasia.Text = "Apelido/Nome Fantasia";
+            // 
+            // clmGenero
+            // 
+            this.clmGenero.Text = "Gênero";
+            // 
+            // clmDataNascimentoCriacao
+            // 
+            this.clmDataNascimentoCriacao.Text = "Data Nasc./Criação";
+            // 
+            // clmCpfCnpj
+            // 
+            this.clmCpfCnpj.Text = "CPF/CNPJ";
+            // 
+            // clmRgInscEstadual
+            // 
+            this.clmRgInscEstadual.Text = "RG/Insc. Estadual";
+            // 
+            // clmEmail
+            // 
+            this.clmEmail.Text = "E-mail";
+            // 
+            // clmTelefone
+            // 
+            this.clmTelefone.Text = "Telefone";
+            // 
+            // clmEndereco
+            // 
+            this.clmEndereco.Text = "Endereço";
+            // 
+            // clmBairro
+            // 
+            this.clmBairro.Text = "Bairro";
+            // 
+            // clmCidade
+            // 
+            this.clmCidade.Text = "Cidade";
+            // 
+            // clmCep
+            // 
+            this.clmCep.Text = "CEP";
+            // 
+            // clmAtivo
+            // 
+            this.clmAtivo.Text = "Ativo";
+            // 
+            // clmInscMunicipal
+            // 
+            this.clmInscMunicipal.Text = "Insc. Municipal";
+            // 
+            // clmInscEstSubTrib
+            // 
+            this.clmInscEstSubTrib.Text = "Insc. Est. Sub. Trib.";
             // 
             // frmConsultaFornecedor
             // 
@@ -42,5 +142,22 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.ColumnHeader clmTipo;
+        private System.Windows.Forms.ColumnHeader clmNomeRazaoSocial;
+        private System.Windows.Forms.ColumnHeader clmApelidoNomeFantasia;
+        private System.Windows.Forms.ColumnHeader clmGenero;
+        private System.Windows.Forms.ColumnHeader clmDataNascimentoCriacao;
+        private System.Windows.Forms.ColumnHeader clmCpfCnpj;
+        private System.Windows.Forms.ColumnHeader clmRgInscEstadual;
+        private System.Windows.Forms.ColumnHeader clmEmail;
+        private System.Windows.Forms.ColumnHeader clmTelefone;
+        private System.Windows.Forms.ColumnHeader clmEndereco;
+        private System.Windows.Forms.ColumnHeader clmBairro;
+        private System.Windows.Forms.ColumnHeader clmCidade;
+        private System.Windows.Forms.ColumnHeader clmCep;
+        private System.Windows.Forms.ColumnHeader clmAtivo;
+        private System.Windows.Forms.ColumnHeader clmInscMunicipal;
+        private System.Windows.Forms.ColumnHeader clmInscEstSubTrib;
     }
 }
--- projeto_patrica/pages/consulta/frmConsultaFuncionario.Designer.cs
+++ projeto_patrica/pages/consulta/frmConsultaFuncionario.Designer.cs
@@ -28,7 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.clmTipo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmNome = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmApelido = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmDataNascimento = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmCpf = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmRg = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmEmail = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmTelefone = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmEndereco = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmBairro = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmCep = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmCidade = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmAtivo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmMatricula = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmCargo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmSalario = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmDataAdmissao = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmTurno = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmCargaHoraria = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmGenero = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
+            // 
+            // listV
+            // 
+            this.listV.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.clmTipo,
+            this.clmNome,
+            this.clmApelido,
+            this.clmDataNascimento,
+            this.clmCpf,
+            this.clmRg,
+            this.clmEmail,
+            this.clmTelefone,
+            this.clmEndereco,
+            this.clmBairro,
+            this.clmCidade,
+            this.clmCep,
+            this.clmAtivo,
+            this.clmMatricula,
+            this.clmCargo,
+            this.clmSalario,
+            this.clmDataAdmissao,
+            this.clmTurno,
+            this.clmCargaHoraria,
+            this.clmGenero});
+            // 
+            // clmTipo
+            // 
+            this.clmTipo.Text = "Tipo Pessoa";
+            // 
+            // clmNome
+            // 
+            this.clmNome.Text = "Nome";
+            // 
+            // clmApelido
+            // 
+            this.clmApelido.Text = "Apelido";
+            // 
+            // clmDataNascimento
+            // 
+            this.clmDataNascimento.Text = "Data de Nasc.";
+            // 
+            // clmCpf
+            // 
+            this.clmCpf.Text = "CPF";
+            // 
+            // clmRg
+            // 
+            this.clmRg.Text = "RG";
+            // 
+            // clmEmail
+            // 
+            this.clmEmail.Text = "E-mail";
+            // 
+            // clmTelefone
+            // 
+            this.clmTelefone.Text = "Telefone";
+            // 
+            // clmEndereco
+            // 
+            this.clmEndereco.Text = "Endereço";
+            // 
+            // clmBairro
+            // 
+            this.clmBairro.Text = "Bairro";
+            // 
+            // clmCep
+            // 
+            this.clmCep.DisplayIndex = 11;
+            this.clmCep.Text = "CEP";
+            // 
+            // clmCidade
+            // 
+            this.clmCidade.DisplayIndex = 12;
+            this.clmCidade.Text = "Cidade";
+            // 
+            // clmAtivo
+            // 
+            this.clmAtivo.Text = "Ativo";
+            // 
+            // clmMatricula
+            // 
+            this.clmMatricula.Text = "Matrícula";
+            // 
+            // clmCargo
+            // 
+            this.clmCargo.Text = "Cargo";
+            // 
+            // clmSalario
+            // 
+            this.clmSalario.Text = "Salário";
+            // 
+            // clmDataAdmissao
+            // 
+            this.clmDataAdmissao.Text = "Data de Admissão";
+            // 
+            // clmTurno
+            // 
+            this.clmTurno.Text = "Turno";
+            // 
+            // clmCargaHoraria
+            // 
+            this.clmCargaHoraria.Text = "Carga Horária";
+            // 
+            // clmGenero
+            // 
+            this.clmGenero.Text = "Gênero";
             // 
             // frmConsultaFuncionario
             // 
@@ -42,5 +168,26 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.ColumnHeader clmTipo;
+        private System.Windows.Forms.ColumnHeader clmNome;
+        private System.Windows.Forms.ColumnHeader clmApelido;
+        private System.Windows.Forms.ColumnHeader clmDataNascimento;
+        private System.Windows.Forms.ColumnHeader clmCpf;
+        private System.Windows.Forms.ColumnHeader clmRg;
+        private System.Windows.Forms.ColumnHeader clmEmail;
+        private System.Windows.Forms.ColumnHeader clmTelefone;
+        private System.Windows.Forms.ColumnHeader clmEndereco;
+        private System.Windows.Forms.ColumnHeader clmBairro;
+        private System.Windows.Forms.ColumnHeader clmCidade;
+        private System.Windows.Forms.ColumnHeader clmCep;
+        private System.Windows.Forms.ColumnHeader clmAtivo;
+        private System.Windows.Forms.ColumnHeader clmMatricula;
+        private System.Windows.Forms.ColumnHeader clmCargo;
+        private System.Windows.Forms.ColumnHeader clmSalario;
+        private System.Windows.Forms.ColumnHeader clmDataAdmissao;
+        private System.Windows.Forms.ColumnHeader clmTurno;
+        private System.Windows.Forms.ColumnHeader clmCargaHoraria;
+        private System.Windows.Forms.ColumnHeader clmGenero;
     }
 }
