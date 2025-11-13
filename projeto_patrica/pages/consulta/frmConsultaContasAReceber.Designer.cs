675ffa13d507ad7e329203c8bdd3fc87eaa7f9bc
Luan Henrique Oliveira Sales <LuanHos1998@hotmail.com>
2025-11-13 15:39:03 -0300

    .

--- projeto_patrica/pages/consulta/frmConsultaContasAReceber.Designer.cs
+++ projeto_patrica/pages/consulta/frmConsultaContasAReceber.Designer.cs
@@ -30,6 +30,16 @@
         {
             this.comboBoxFiltroStatus = new System.Windows.Forms.ComboBox();
             this.lblFiltros = new System.Windows.Forms.Label();
+            this.clmModelo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmSerie = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmNumNota = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmIdCliente = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmCliente = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmValorParcela = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmFormaPagamento = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmDataEmissao = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmDataVencimento = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmAtivo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // btnExcluir
@@ -37,6 +47,25 @@
             this.btnExcluir.Location = new System.Drawing.Point(1125, 617);
             this.btnExcluir.Size = new System.Drawing.Size(99, 34);
             this.btnExcluir.Text = "Cancelar Conta";
+            // 
+            // listV
+            // 
+            this.listV.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.clmModelo,
+            this.clmSerie,
+            this.clmNumNota,
+            this.clmIdCliente,
+            this.clmCliente,
+            this.clmValorParcela,
+            this.clmFormaPagamento,
+            this.clmDataEmissao,
+            this.clmDataVencimento,
+            this.clmAtivo});
+            // 
+            // clmCod
+            // 
+            this.clmCod.Text = "Num. Parcela";
+            this.clmCod.Width = 120;
             // 
             // btnAlterar
             // 
@@ -71,6 +100,61 @@
             this.lblFiltros.TabIndex = 12;
             this.lblFiltros.Text = "Filtros de Parcelas";
             // 
+            // clmModelo
+            // 
+            this.clmModelo.Text = "Modelo";
+            this.clmModelo.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.clmModelo.Width = 80;
+            // 
+            // clmSerie
+            // 
+            this.clmSerie.Text = "Série";
+            this.clmSerie.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.clmSerie.Width = 80;
+            // 
+            // clmNumNota
+            // 
+            this.clmNumNota.Text = "Nº Nota";
+            this.clmNumNota.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.clmNumNota.Width = 150;
+            // 
+            // clmIdCliente
+            // 
+            this.clmIdCliente.Text = "Cód. Cliente";
+            this.clmIdCliente.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.clmIdCliente.Width = 100;
+            // 
+            // clmCliente
+            // 
+            this.clmCliente.Text = "Cliente";
+            this.clmCliente.Width = 250;
+            // 
+            // clmValorParcela
+            // 
+            this.clmValorParcela.Text = "Valor da Parcela";
+            this.clmValorParcela.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.clmValorParcela.Width = 100;
+            // 
+            // clmFormaPagamento
+            // 
+            this.clmFormaPagamento.Text = "Forma de Pagamento";
+            this.clmFormaPagamento.Width = 150;
+            // 
+            // clmDataEmissao
+            // 
+            this.clmDataEmissao.Text = "Data Emissão";
+            this.clmDataEmissao.Width = 120;
+            // 
+            // clmDataVencimento
+            // 
+            this.clmDataVencimento.Text = "Data Vencimento";
+            this.clmDataVencimento.Width = 120;
+            // 
+            // clmAtivo
+            // 
+            this.clmAtivo.Text = "Status";
+            this.clmAtivo.Width = 80;
+            // 
             // frmConsultaContasAReceber
             // 
             this.Controls.Add(this.comboBoxFiltroStatus);
@@ -96,5 +180,15 @@
 
         private System.Windows.Forms.ComboBox comboBoxFiltroStatus;
         private System.Windows.Forms.Label lblFiltros;
+        private System.Windows.Forms.ColumnHeader clmModelo;
+        private System.Windows.Forms.ColumnHeader clmSerie;
+        private System.Windows.Forms.ColumnHeader clmNumNota;
+        private System.Windows.Forms.ColumnHeader clmIdCliente;
+        private System.Windows.Forms.ColumnHeader clmCliente;
+        private System.Windows.Forms.ColumnHeader clmValorParcela;
+        private System.Windows.Forms.ColumnHeader clmFormaPagamento;
+        private System.Windows.Forms.ColumnHeader clmDataEmissao;
+        private System.Windows.Forms.ColumnHeader clmDataVencimento;
+        private System.Windows.Forms.ColumnHeader clmAtivo;
     }
 }
